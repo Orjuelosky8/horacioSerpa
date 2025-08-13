700b010bec65a28773166a0de31f050ffb4e617f
maikol vergara <afiliaciones.tuapo@gmail.com>
2025-08-13 20:39:33 +0000

    dime que debo hacer par acada red social ? para poderlo integrar bien

--- src/components/sections/live-cubesphere.tsx
+++ src/components/sections/live-cubesphere.tsx
@@ -34,7 +34,7 @@ export default function LiveCubesphere() {
                   className="instagram-media" 
                   data-instgrm-permalink="https://www.instagram.com/p/C7_QY2vRUeK/" 
                   data-instgrm-version="14"
-                  style={{ background: '#FFF', border: '0', borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)', margin: '1px', maxWidth: '540px', minWidth: '326px', padding: '0', width: '99.375%' }}>
+                  style={{ background: '#FFF', border: '0', borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)', margin: '1px', maxWidth: '540px', minWidth: '326px', padding: '0', width: 'calc(100% - 2px)' }}>
                 </blockquote>
               </CardContent>
             </Card>
